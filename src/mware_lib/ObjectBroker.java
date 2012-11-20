7d5f583ed8c2cfc1ad6b0df766c4283bc0fe62e5
kaib <kai.bielenberg@haw-hamburg.de>
2012-11-20 10:31:47 +0100

    all fine now

--- src/mware_lib/ObjectBroker.java
+++ src/mware_lib/ObjectBroker.java
@@ -7,16 +7,8 @@ public class ObjectBroker {
 	// Parameter: Host und Port, bei dem die Dienste (Namensdienst)
 	// kontaktiert werden sollen.
 	public static ObjectBroker getBroker(String serviceHost, int listenPort) { 
-		
-<<<<<<< HEAD
 			return null;
-=======
-<<<<<<< HEAD
-			return null;
-=======
-			return new Object();
->>>>>>> origin/master
->>>>>>> e781f7fb2013ee933d124d66fc4acacb6313ca30
+
 			}
 	// Liefert den Namensdienst (Stellvetreterobjekt).
 	public NameService getNameService() {
