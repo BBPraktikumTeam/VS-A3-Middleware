4391bf126b262fb4b5dffb3616c7c4eee0cab872
kaib <kai.bielenberg@haw-hamburg.de>
2012-11-20 09:52:52 +0100

    init classes

---
+++ src/cash_access/OverdraftException.java
@@ -0,0 +1,12 @@
+package cash_access;
+
+public class OverdraftException extends Exception {
+
+	public OverdraftException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public OverdraftException(String message) {
+		
+	}
+}
---
+++ src/mware_lib/ObjectBroker.java
@@ -0,0 +1,21 @@
+package mware_lib;
+
+public abstract class ObjectBroker {
+
+	// Das hier zurückgelieferte Objekt soll der zentrale Einstiegspunkt
+	// der Middleware aus Anwendersicht sein.
+	// Parameter: Host und Port, bei dem die Dienste (Namensdienst)
+	// kontaktiert werden sollen.
+	public static ObjectBroker getBroker(String serviceHost, int listenPort) { 
+		
+			return new ObjectBroker();
+			}
+	// Liefert den Namensdienst (Stellvetreterobjekt).
+	public NameService getNameService() {
+			
+		
+		return new NameService();
+	}
+		
+	
+}
