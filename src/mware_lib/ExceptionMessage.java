1a32ce8850c0507e997278ff5c5cf5c6e213aa94
oliver <oliver.behncke@haw-hamburg.de>
2012-11-28 15:39:19 +0100

    Messages get created respecting their own class

--- src/mware_lib/ExceptionMessage.java
+++ src/mware_lib/ExceptionMessage.java
@@ -27,6 +27,23 @@ public class ExceptionMessage extends ReplyMessage {
 	public boolean result() {
 		return false;
 	}
+	
+	public void throwAsException() {
+		Object o=null;
+		try {
+			final Class<?>[] CONSTRUCTOR_SIGNATURE = {
+					Class.forName("java.lang.String") };
+			final Object[] CONSTRUCTOR_ARGS = { message};
+				o= Class.forName(type.substring(6)).getConstructor(CONSTRUCTOR_SIGNATURE).newInstance(CONSTRUCTOR_ARGS);
+
+		} catch (Exception e) {
+		}
+		if(o==null || !(o instanceof RuntimeException)){
+			throw new RuntimeException(message);
+		}
+		else
+			throw ((RuntimeException) o);
+	}
 
 	@Override
 	public String toString() {
--- test/mware_lib/TestExceptionMessage.java
+++ test/mware_lib/TestExceptionMessage.java
@@ -8,11 +8,32 @@ public class TestExceptionMessage {
 
 	@Test
 	public void testExceptionMessage() {
-		ExceptionMessage excMsg = new ExceptionMessage("exception,42,java.lang.RuntimeException,SomeText");
+		ExceptionMessage excMsg = new ExceptionMessage("exception,42,class java.lang.RuntimeException,SomeText");
 		assertEquals(42, excMsg.messageId());
-		assertEquals("java.lang.RuntimeException", excMsg.type());
+		assertEquals("class java.lang.RuntimeException", excMsg.type());
 		assertEquals("SomeText", excMsg.message());
 		assertTrue(excMsg.exception());
 		assertFalse(excMsg.result());
+	}
+	
+	@Test
+	public void testThrowAsException(){
+		ExceptionMessage excMsg= new ExceptionMessage("exception,42,class java.lang.NumberFormatException,SomeText");
+		try{
+			excMsg.throwAsException();
+			fail("Exception not thrown");
+		}
+		catch (NumberFormatException e){
+			assertTrue(true);
+		} catch (Exception e){
+			fail("wrong exception thrown");
+		}
+		ExceptionMessage excMsg2= new ExceptionMessage("exception,42,class java.lang.IllegalAccessException,SomeText");
+		try{
+			excMsg2.throwAsException();
+			fail("Exception not thrown");
+		} catch (Exception e){
+			assertTrue(e.getClass().toString().equals("class java.lang.RuntimeException"));
+		}
 	}
 }
